90bf52d081e7130845b6e1c8a9046d1b8a8a376a
maii483 <mai_m00433@cic-cairo.com>
2023-07-14 19:16:36 -0700

    BA7EBEKK

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 
 /**
- * main - Prints the alphabetic in lower and upper case
- *
- * Return: Always (Success)
- */
+  * main - Prints some letters of alphabet
+  *
+  * Return: Always (Success)
+  */
 int main(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		putchar(c);
-	}
-
-	for (c = 'A'; c <= 'Z'; c++)
-	{
-		putchar(c);
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
 	}
 
 	putchar('\n');
